a70c76209fc63e36cb5ae6532b9384652609201d
Vinny Martinez <vinny@vinnymartinez.com>
2017-01-20 15:48:23 -0500

    cool bugz

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt(){
+  $("p").click(function(){
+    alert("hey!")
+  })
+}
+
+function frameIt(){
+  $(window).on("load", function(){
+    $("img").addClass('tasty')
+  });
+}
+
+function pressIt(){
+  $(document).on("keypress", function(key){
+    if (key.which == 103){
+      alert('g was pressed')
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+$(function(){
 
 // call functions here
 
-});
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
+})
